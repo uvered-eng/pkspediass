f1d1a85e7e4e095326b2df04eebb4112c1580add
uvered <63880893+uvered-eng@users.noreply.github.com>
2021-04-29 15:36:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+function red(){
+    location.href="https://exe.io/YWa2XL";
+}
+
+function blue(){
+    location.href="https://exe.io/b0sj";
+}
+
+function yellow(){
+    location.href="https://exe.io/3kSNZDz9";
+}
+function oro(){
+    location.href="https://exe.io/XTbWQAPY"
+}
+function plata(){
+    location.href="https://exe.io/h5Vdog"
+}
+function cristal(){
+    location.href="https://exe.io/Shlc4"
+}
+function rubi(){
+    location.href="https://exe.io/rLcRT"
+}
+function safiro(){
+    location.href="https://exe.io/7Rmc"
+}
+function esmeralda(){
+    location.href="https://exe.io/YuXJNyfe"
+}
+function perla(){
+    location.href="https://exe.io/oTTW4i"
+}
+function diamante(){
+    location.href="https://exe.io/l2ukX"
+}
+function platinum(){
+    location.href="https://exe.io/wSLoi"
+}
+function black(){
+    location.href="https://exe.io/AN927i"
+}
+function white(){
+    location.href="https://exe.io/HUV7aVf5"
+}
+function black2(){
+    location.href="https://exe.io/RU7NZw47"
+}
+function white2(){
+    location.href="https://exe.io/LC0sjB"
+}
+function heartgold(){
+    location.href="https://exe.io/wqLhNF"
+}
+function soulsilver(){
+    location.href="https://exe.io/oZ2GjsiU"
+}
+function y(){
+    location.href="https://exe.io/jjuv"
+}
+function x(){
+    location.href="https://exe.io/nlcb"
+}
+function omega(){
+    location.href="https://exe.io/QLSQA"
+}
+function alpha(){
+    location.href="https://exe.io/xslBGIKl"
+}
+function sun(){
+    location.href="https://exe.io/z52z"
+}
+function moon(){
+    location.href="https://exe.io/z52z"
+}
+function ultrasun(){
+    location.href="https://exe.io/z52z"
+}
+function ultramoon(){
+    location.href="https://exe.io/z52z"
+}
